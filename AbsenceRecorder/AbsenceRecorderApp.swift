93bda6a1af5699a52a302fcd8fd5dd0b4f9e4815
Dylan Guo <guo.z@etoncollege.org.uk>
2023-01-20 22:30:00 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Guo, Dylan (Coll) on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
